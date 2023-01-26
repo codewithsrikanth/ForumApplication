0773861144f737d0b80101502e8611040b3a7169
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2023-01-26 09:41:07 +0530

    Votes Repository Implmentation

--- ForumApplication.Repositories/VotesRepository.cs
+++ ForumApplication.Repositories/VotesRepository.cs
@@ -1,10 +1,46 @@
-﻿namespace ForumApplication.Repositories
+﻿using ForumApplication.DataModels;
+using System.Linq;
+
+namespace ForumApplication.Repositories
 {
     public interface IVotesRepository
     {
-
+        void UpdateVote(int aid, int uid, int value);
     }
     public class VotesRepository:IVotesRepository
     {
+        private ForumAppDbContext _dbContext;
+        public VotesRepository()
+        {
+            _dbContext = new ForumAppDbContext();
+        }
+
+        public void UpdateVote(int aid, int uid, int value)
+        {
+            int updateValue;
+            if (value > 0)
+                updateValue = 1;
+            else if (value < 0)
+                updateValue = -1;
+            else
+                updateValue = 0;
+
+            var vote = _dbContext.Votes.Where(x => x.AnswerID == aid && x.UserID == uid).FirstOrDefault();
+            if(vote != null)
+            {
+                vote.VoteValue = updateValue;
+            }
+            else
+            {
+                Vote newVote = new Vote()
+                {
+                    UserID = uid,
+                    AnswerID = aid,
+                    VoteValue = updateValue
+                };
+                _dbContext.Votes.Add(newVote);
+            }
+            _dbContext.SaveChanges();
+        }
     }
 }
